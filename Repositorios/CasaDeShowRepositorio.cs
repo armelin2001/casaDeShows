d12b534d0c2f50912b23eb8d9c72a649a0dbb392
armelin2001 <matheusarmelindeolivera@gmail.com>
2020-02-18 10:36:53 -0300

    Criando repositorios
    Fazendo a criação dos repositorios para as alterações do bd só poderem ser feitas no repositorio, logo iremos utilizar os repositorios nas controlers

--- Repositorios/CasaDeShowRepositorio.cs
+++ Repositorios/CasaDeShowRepositorio.cs
@@ -1,7 +1,36 @@
+using casaDeShows.Data;
+using casaDeShows.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace casaDeShows.Repositorios
 {
     public class CasaDeShowRepositorio
     {
-        
+        private readonly ApplicationDbContextEntidades _dataBase;
+        public CasaDeShowRepositorio(ApplicationDbContextEntidades dataBase){
+            _dataBase = dataBase;
+        }
+        public CasaDeShow BuscarCasaDeShow(int id){
+            var buscaCasaDeshow = _dataBase.CasaDeShows.FirstOrDefault(casa => casa.Id == id);
+            return buscaCasaDeshow;
+        }
+        public List<CasaDeShow> MostrarCasasDeShow(){
+            return _dataBase.CasaDeShows.ToList();
+        }
+        public void AdicionarCasaDeShow(CasaDeShow casa){
+            _dataBase.CasaDeShows.Add(casa);
+            _dataBase.SaveChanges();
+        }
+        public void EditarCasaDeShow(CasaDeShow casa){
+            _dataBase.CasaDeShows.Update(casa);
+            _dataBase.SaveChanges();
+        }
+        public void DeletarCasaDeShows(CasaDeShow casa){
+            _dataBase.CasaDeShows.Remove(casa);
+            _dataBase.SaveChanges();
+        }
     }
 }
--- Repositorios/EventoRepositorio.cs
+++ Repositorios/EventoRepositorio.cs
@@ -1,7 +1,52 @@
+using casaDeShows.Data;
+using casaDeShows.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace casaDeShows.Repositorios
 {
     public class EventoRepositorio
     {
+        private readonly ApplicationDbContextEntidades _dataBase;
+        public EventoRepositorio(ApplicationDbContextEntidades dataBase){
+            _dataBase = dataBase;
+        }
+        //Metodos para listagem de objetos
+        public List<Evento> MostrarEventos(){
+            return _dataBase.Eventos.ToList();
+        }
+        public List<GeneroEvento> MostrarGenerosEventos(){
+            return _dataBase.GeneroEventos.ToList();
+        }
+        //Chamar o list de casa de show no controller
         
+        //Methodos para busca de objetos 
+        public Evento BuscarGeneroDoEvento(GeneroEvento idGenero){//testar em uma view para ver se conseguimos pegar um gero de evento direto
+            var buscaGeneroDoEvento = _dataBase.Eventos.FirstOrDefault(encontraGenero => encontraGenero.IdGeneroDoEvento
+            == idGenero);
+            return buscaGeneroDoEvento;
+        }
+        public Evento BuscarObjetoCasaDeShow(CasaDeShow idCasaDeShow){
+            var buscaObjeto = _dataBase.Eventos.FirstOrDefault(encontraObjeto => encontraObjeto.IdCasaDeShows == idCasaDeShow);
+            return buscaObjeto;
+        }
+        public Evento BuscarEvento(int id){
+            var buscaEventoId = _dataBase.Eventos.FirstOrDefault(encontrandoEvento => encontrandoEvento.Id == id);
+            return buscaEventoId; 
+        }
+        public void AdicionarEventos(Evento eventoAdd){
+            _dataBase.Eventos.Add(eventoAdd);
+            _dataBase.SaveChanges();
+        }
+        public void EditarEvento(Evento editaEvento){
+            _dataBase.Eventos.Update(editaEvento);
+            _dataBase.SaveChanges();
+        }
+        public void DeletarEventos(Evento deletaEvento){
+            _dataBase.Eventos.Remove(deletaEvento);
+            _dataBase.SaveChanges();
+        }
     }
 }
